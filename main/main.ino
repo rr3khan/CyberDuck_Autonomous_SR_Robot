c6f6ae58d5d5eb5af37dcb960287b4add9542f98
rr3khan <65298773+rr3khan@users.noreply.github.com>
2022-02-24 23:13:26 -0500

    Added preliminary set up for ultra sonic sensors

--- main/main.ino
+++ main/main.ino
@@ -5,6 +5,27 @@
 #include <AFMotor.h> // Library for Adafruit motor shield  
 #include <NewPing.h> // Library for the Ultrasonic sensor
 
+// Setup Pins for UltraSonic Sensors
+
+// To do determine which pins these are on the board
+
+#define TRIGGER_PIN_F A0
+#define ECHO_PIN_F    A1
+#define TRIGGER_PIN_L A2
+#define ECHO_PIN_L    A3
+#define TRIGGER_PIN_R A4 
+#define ECHO_PIN_R    A5
+
+// To do determine the max distance we need to work with
+
+#define MAX_DISTANCE 300
+
+// Setup each of the UltraSonic sensors
+
+NewPing sonarF(TRIGGER_PIN_F, ECHO_PIN_F, MAX_DISTANCE);
+NewPing sonarL(TRIGGER_PIN_L, ECHO_PIN_L, MAX_DISTANCE);
+NewPing sonarR(TRIGGER_PIN_R, ECHO_PIN_R, MAX_DISTANCE);
+
 // initialize motors
 // to do determine what PWM frequencies to use for motors
 
@@ -21,6 +42,15 @@ void turnRight() {
   motorBL.run(FORWARD);
   motorFL.run(FORWARD);
 }
+
+void moveForward() {
+  motorBR.run(FORWARD);
+  motorFR.run(FORWARD);
+  motorBL.run(FORWARD);
+  motorFL.run(FORWARD);
+}
+
+// Reading values from the UltraSonic sensor
 
 
 
